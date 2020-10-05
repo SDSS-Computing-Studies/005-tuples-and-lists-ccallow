7dffa1936ffc1427f1021b450ac450bb19d8f4fd
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-05 12:24:57 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -14,3 +14,7 @@ The smallest number is 3 and the largest number is 9
 """
 
 myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
+myList.sort()
+a = str(myList[0])
+b = str(myList[-1])
+print("The smallest number is " + a + " and the largest number is "+ b + ".")
